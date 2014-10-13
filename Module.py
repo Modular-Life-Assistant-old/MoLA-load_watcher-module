a2a687b0fb186153de38beeb168e504e8e306147
Yoann Ono Dit Biot <y0no@y0no.fr>
2014-10-13 16:17:06 +0200

    Use Circuits lib

--- Module.py
+++ Module.py
@@ -1,32 +1,34 @@
 from core import Log
-from core import EventManager
 
+from circuits import Component, Event, Timer
 import subprocess
-import time
 
-class Module:
-    def __init__(self):
-        self.__max_average = int(subprocess.check_output([
-            'grep',
-            '-c',
-            '^processor',
-            '/proc/cpuinfo'
-        ]))
 
-    def thread_check(self):
-        while True:
-            average = float(subprocess.check_output([
-                'cat',
-                '/proc/loadavg'
-            ]).split()[0])
+class load_alert(Event):
+    """ Event raised when load is to high
+    """
+    def __init__(self, load, load_max):
+        super(load_alert, self).__init__(load, load_max)
 
-            if average > self.__max_average:
-                message = 'Average at %f (max: %d)' % (average, self.__max_average)
-                Log.warning(message)
-                EventManager.trigger(
-                    'perf_alert',
-                    message=message,
-                )
 
-            time.sleep(60)
+class Module(Component):
+    channel = 'load_avg'
+    delay = 60
+    max_avg = 0
+
+    def get_load_avg(self):
+        avg_cmd = ['cat', '/proc/loadavg']
+        output = subprocess.check_output(avg_cmd)
+        avg = float(output.split()[0])
+
+        if avg >= self.max_avg:
+            Log.warning('Average at %f (max: %d)' % (avg, self.max_avg))
+            self.fire(load_alert(avg, self.max_avg))
+
+    def started(self, component):
+        process_cmd = ['grep', '-c', '^processor', '/proc/cpuinfo']
+        cpu_count = subprocess.check_output(process_cmd)
+        self.max_avg = int(cpu_count)
+        load_avg_evt = Event.create('get_load_avg')
+        Timer(self.delay, load_avg_evt, self.channel, persist=True).register(self)
 
