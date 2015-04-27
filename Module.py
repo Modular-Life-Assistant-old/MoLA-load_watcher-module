6a50ecc0265586697c3dc618d64869a8a05de356
Treemo <treemo@hotmail.fr>
2015-04-27 18:11:04 +0200

    use os lib

--- Module.py
+++ Module.py
@@ -1,7 +1,7 @@
 from core import Log
 
 from circuits import Component, Event, Timer
-import subprocess
+import os
 import time
 
 
@@ -18,18 +18,13 @@ class Module(Component):
     max_avg = 0
 
     def get_load_avg(self):
-        avg_cmd = ['cat', '/proc/loadavg']
-        output = subprocess.check_output(avg_cmd)
-        avg = float(output.split()[0])
+        avg = os.getloadavg()[0]
 
         if avg >= self.max_avg:
             Log.warning('Average at %f (max: %d)' % (avg, self.max_avg))
             self.fire(load_alert(avg, self.max_avg, time.time()))
 
-    def started(self, component):
-        process_cmd = ['grep', '-c', '^processor', '/proc/cpuinfo']
-        cpu_count = subprocess.check_output(process_cmd)
-        self.max_avg = int(cpu_count)
+    def started(self, c):
+        self.max_avg = os.cpu_count()
         load_avg_evt = Event.create('get_load_avg')
         Timer(self.delay, load_avg_evt, self.channel, persist=True).register(self)
-
